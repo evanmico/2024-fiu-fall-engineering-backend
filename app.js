620f0f3a2776bcf7493c56275b17331269d571f0
evanmico <evanmico222@gmail.com>
2023-12-06 14:15:03 -0500

    setup ENV so that the database can be queried without leaking creds finally

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import connectDB from './config/dbConn.js';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res)=>{
+  res.json("Hello this is the backend");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get("/account", (req, res)=>{
+  const q = 'SELECT * FROM account';
+  connectDB.query(q,(err,data)=>{
+    if(err) return res.json(err);
+    return res.json(data);
+  })
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post("/account", (req,res)=>{
+  const q = "INSERT INTO account (`email`, `password`, `accountTypeID`) VALUES (?)";
+  const values = [ 
+    req.body.email,
+    req.body.password,
+    req.body.accountTypeID
+  ];
+
+  connectDB.query(q,[values], (err, data)=>{
+    if(err) return res.json(err);
+    return res.json('Account added successfully');
+  })
+})
+
+app.listen(8800, ()=>{
+  console.log("Connected to backend!");
 });
-
-module.exports = app;
